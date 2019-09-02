597ebb4e0c096cf52224e8505963ad7c75733aa3
theodoreOnzGit <tong011@e.ntu.edu.sg>
2019-09-02 15:37:37 -0700

    hello world added

---
+++ basicCode/hello_world.cpp
@@ -0,0 +1,11 @@
+// A hello world program in C++
+#include<iostream>
+using namespace std;
+
+int main()
+	{
+	cout << "Hello World!";
+	return 0;
+	}
+
+// credit: https://www.codingunit.com/cplusplus-tutorial-first-cplusplus-program-hello-world
