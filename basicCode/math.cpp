889d3586c1eeb9078e71a3ea4a0bfc42cb1cd691
theodoreOnzGit <tong011@e.ntu.edu.sg>
2019-09-03 10:54:34 -0700

    added basic math addition functions

---
+++ basicCode/math.cpp
@@ -0,0 +1,17 @@
+// A hello world program in C++
+#include<iostream>
+using namespace std;
+
+int main()
+	{
+	double a,b;
+	cout << "now we're going to add a + b" << endl;
+	cout << "please state a value for a:   ";
+	cin >> a;
+	cout << "please state a value for b:   ";
+	cin >> b;
+	cout << a+b << endl;
+	return 0;
+	}
+
+// credit: https://www.codingunit.com/cplusplus-tutorial-first-cplusplus-program-hello-world
