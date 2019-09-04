601b8195f1d8c9c8fa7a1f15f65f7c4c2e574142
theodoreOnzGit <tong011@e.ntu.edu.sg>
2019-09-03 21:32:01 -0700

    more basic math added

--- basicCode/math.cpp
+++ basicCode/math.cpp
@@ -1,24 +1,28 @@
 // A hello world program in C++
 #include<iostream>
 #include<string>
+#include<cmath>
+
 using namespace std;
 
 int main()
 	{
-	float a,b;
+	float m,v;
 	string first_statement,second_statement,third_statement;
 
-	first_statement =  "now we're going to add a + b";
-	second_statement = "please state a value for a:   ";
-	third_statement = "please state a value for b:   ";
+	first_statement =  "now we're going to calculate KE=1/2*m*v^2";
+	second_statement = "please state a value for mass (kg):   ";
+	third_statement = "please state a value for velocity (m/s):   ";
 
 	cout << first_statement << endl;
 	cout << second_statement;
-	cin >> a;
+	cin >> m;
 	cout << third_statement ;
-	cin >> b;
-	cout << a+b << endl;
-	cout << 9*9 << endl;
+	cin >> v;
+	cout << (1.0/2.0)*m*pow(v,2)  << endl;
+
+	cout << sqrt(2.0) << endl;
+	cout << pow(2.0,0.5) << endl;
 	return 0;
 	}
 
