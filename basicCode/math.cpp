333c308adafcabe33f50715af459cfe86d712b4d
theodoreOnzGit <tong011@e.ntu.edu.sg>
2019-09-03 20:54:39 -0700

    basic addition discussed

--- basicCode/math.cpp
+++ basicCode/math.cpp
@@ -1,16 +1,24 @@
 // A hello world program in C++
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main()
 	{
-	double a,b;
-	cout << "now we're going to add a + b" << endl;
-	cout << "please state a value for a:   ";
+	float a,b;
+	string first_statement,second_statement,third_statement;
+
+	first_statement =  "now we're going to add a + b";
+	second_statement = "please state a value for a:   ";
+	third_statement = "please state a value for b:   ";
+
+	cout << first_statement << endl;
+	cout << second_statement;
 	cin >> a;
-	cout << "please state a value for b:   ";
+	cout << third_statement ;
 	cin >> b;
 	cout << a+b << endl;
+	cout << 9*9 << endl;
 	return 0;
 	}
 
