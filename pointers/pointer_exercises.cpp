5c840335e285a24340afeb13c30f0e247428148d
theodoreOnzGit <tong011@e.ntu.edu.sg>
2019-09-26 09:21:09 -0700

    some pointer exercises added

---
+++ pointers/pointer_exercises.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	double vector[3][1];
+	vector[0][0]=1;
+	vector[1][0]=2;
+	vector[2][0]=3;
+	// so we've just defined a vector
+	
+	cout << vector[0][0] << " this is the first value" << endl;
+	cout << vector[1][0] << " this is the second value" << endl;
+	cout << vector[2][0] << " this is the third value" << endl;
+	
+	cout << vector << " this is the pointer address" << endl;
+
+	cout << endl;
+	cout << "let's play with individual pointers first" << endl;
+
+	double a;
+	a=1;
+	cout << &a << endl;
+	cout << "The above prints the individual address of a." << endl;
+
+
+	cout << "now we want to play with pointer variables" << endl;
+	double * pointerA;
+	pointerA = &a;
+	cout << pointerA << " This is the value of pointerA, which we just declared " << endl;
+	cout << endl;
+	cout << "Now we want to print out the value of pointerA." << endl;
+
+	cout << *pointerA << endl;
+	cout << "pointerA value is stated above..." << endl;
+
+
+
+	return 0;
+}
+
---
+++ pointers/vector_addition.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+double vector_length(double x, double y, double z);
+double vector_add(double vectorA[],double vectorB[]);
+
+int main ()
+{
+	double l;
+	double vector1[3][1];
+	
+	cout << "we are adding vector 1 to vector 2, they each have 3 rows and 1 col ... " << endl;
+	std::cout << "vector 1 input x " << std::endl;
+	std::cin >> vector1[0][0];
+
+	std::cout << "vector 1 input y " << std::endl;
+	std::cin >> vector1[1][0];
+
+	std::cout << "vector 1 input z "<< std::endl;
+	std::cin >> vector1[2][0];
+	
+	double vector2[3][1];
+	cout << "vector 2 input x " << endl;
+	cin >> vector2[0][0];
+
+	cout << "vector 2 input y " << endl;
+	cin >> vector2[1][0];
+
+	cout << "vector 2 input z " << endl;
+	cin >> vector2[2][0];
+
+
+	// calc length
+	// std::cout << "the length is " << std::endl;
+	// l = vector_length(vector[0][0],vector[1][0],vector[2][0]);
+	// debugging functions outputting array may be difficult, let's try something
+	// easier first	
+	// double add_result[3][1] = vector_add(vector1,vector2);
+	double add_result[3][1];
+	for (int i = 0; i < 3 ; i++){
+	
+	add_result[i][0] = vector1[i][0] + vector2[i][0];
+
+	cout << "the element in row " << i << " and column 0 is ...  " << endl;
+
+	cout << add_result[i][0] << endl;
+	
+	}
+
+	//std::cout << l << std::endl;
+
+
+	return 0;
+}
+
+double vector_length(double x, double y, double z)
+{
+	double l;
+	l = sqrt(pow(x,2.0)+pow(y,2.0)+pow(z,2.0));
+	return l;
+}
+/*
+double vector_add(double vectorA, double vectorB)
+{
+	double vector_add_result[3][1];
+
+	//vector_add_result[] = vectorA[];
+	for (int i = 0, i < 3, i++)
+	{
+		vector_add_result[i][0] = vector1[i][0];
+	}	
+
+	return vector_add_result[];
+	
+}
+
+*/
+
+
